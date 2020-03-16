021fbf74e46014086eb890e1e936e92b41671c29
Tatiana Levochko <tg.levochko@gmail.com>
2020-03-16 14:55:29 +0200

    add nodes and pricing

--- VelasTest.go
+++ VelasTest.go
@@ -28,10 +28,10 @@ var (
 )
 
 // EthdepositcontractABI is the input ABI used to generate the binding from.
-const EthdepositcontractABI = "[{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"resource\",\"type\":\"uint256\"}],\"name\":\"deposit\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"hash\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"sum\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"voices\",\"type\":\"uint256\"}],\"name\":\"createInvoice\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"user\",\"type\":\"address\"}],\"name\":\"withdraw\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+const EthdepositcontractABI = "[{\"inputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"fallback\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_keepPerByte\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"_writePerByte\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"_GPUTPerCycle\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"_CPUTtPerCycle\",\"type\":\"uint256\"}],\"name\":\"proposePricing\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address[]\",\"name\":\"_nodes\",\"type\":\"address[]\"}],\"name\":\"depositWithNodes\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"height_start\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"height_end\",\"type\":\"uint256\"},{\"internalType\":\"address\",\"name\":\"user\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"keepPerByte\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"writePerByte\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"GPUTPerCycle\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"CPUTtPerCycle\",\"type\":\"uint256\"}],\"name\":\"createInvoice\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"registerNode\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"withdraw\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 
 // EthdepositcontractBin is the compiled bytecode used for deploying new contracts.
-var EthdepositcontractBin = "0x608060405234801561001057600080fd5b50610147806100206000396000f3fe6080604052600436106100345760003560e01c80630e7ccabc1461003957806351cff8d914610088578063b6b55f25146100d9575b600080fd5b34801561004557600080fd5b506100866004803603606081101561005c57600080fd5b81019080803590602001909291908035906020019092919080359060200190929190505050610107565b005b34801561009457600080fd5b506100d7600480360360208110156100ab57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919050505061010c565b005b610105600480360360208110156100ef57600080fd5b810190808035906020019092919050505061010f565b005b505050565b50565b5056fea265627a7a72315820e132f1da32c5c74b147af6ab96a02d830b4e28c701b25c62035a3dde4ad86c2a64736f6c63430005100032"
+var EthdepositcontractBin = "0x608060405234801561001057600080fd5b506001600080018190555060016000600101819055506001600060020181905550600160006003018190555060008060040160006101000a81548160ff0219169083151502179055506105f8806100686000396000f3fe60806040526004361061004a5760003560e01c80633296972f1461010057806351cff8d914610159578063672d7a0d146101aa57806376c58850146101fb57806396ea8630146102b3575b6000600660003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000209050348160060160008282540192505081905550600081600001600082015481600001556001820154816001015560028201548160020155600382015481600301556004820160009054906101000a900460ff168160040160006101000a81548160ff02191690831515021790555090505050005b34801561010c57600080fd5b506101576004803603608081101561012357600080fd5b8101908080359060200190929190803590602001909291908035906020019092919080359060200190929190505050610340565b005b34801561016557600080fd5b506101a86004803603602081101561017c57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506103d8565b005b3480156101b657600080fd5b506101f9600480360360208110156101cd57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610485565b005b6102b16004803603602081101561021157600080fd5b810190808035906020019064010000000081111561022e57600080fd5b82018360208201111561024057600080fd5b8035906020019184602083028401116401000000008311171561026257600080fd5b919080806020026020016040519081016040528093929190818152602001838360200280828437600081840152601f19601f8201169050808301925050505050505091929192905050506104e3565b005b3480156102bf57600080fd5b5061033e600480360360e08110156102d657600080fd5b810190808035906020019092919080359060200190929190803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291908035906020019092919080359060200190929190803590602001909291905050506105ba565b005b6000600660003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002090508481600001600001819055508381600001600101819055508281600001600201819055508181600001600301819055506001600060040160006101000a81548160ff0219169083151502179055505050505050565b6000600560008373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000209050600081600101541161042c57600080fd5b3373ffffffffffffffffffffffffffffffffffffffff166108fc82600101549081150290604051600060405180830381858888f19350505050158015610476573d6000803e3d6000fd5b50600081600101819055505050565b6001600560008373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060020160006101000a81548160ff02191690831515021790555050565b6000600660003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020905034816006016000828254019250508190555060001515600060040160009054906101000a900460ff16151514156105b657600081600001600082015481600001556001820154816001015560028201548160020155600382015481600301556004820160009054906101000a900460ff168160040160006101000a81548160ff0219169083151502179055509050505b5050565b5050505050505056fea265627a7a7231582013a2774eeae4215a6d8898621b795e8f9ee68ce1f8008ff41ca38d7db43d63e064736f6c63430005100032"
 
 // DeployEthdepositcontract deploys a new Ethereum contract, binding an instance of Ethdepositcontract to it.
 func DeployEthdepositcontract(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Ethdepositcontract, error) {
@@ -189,65 +189,107 @@ func (_Ethdepositcontract *EthdepositcontractTransactorRaw) Transact(opts *bind.
 	return _Ethdepositcontract.Contract.contract.Transact(opts, method, params...)
 }
 
-// CreateInvoice is a paid mutator transaction binding the contract method 0x0e7ccabc.
+// CreateInvoice is a paid mutator transaction binding the contract method 0x96ea8630.
 //
-// Solidity: function createInvoice(bytes32 hash, uint256 sum, uint256 voices) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactor) CreateInvoice(opts *bind.TransactOpts, hash [32]byte, sum *big.Int, voices *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.contract.Transact(opts, "createInvoice", hash, sum, voices)
+// Solidity: function createInvoice(uint256 height_start, uint256 height_end, address user, uint256 keepPerByte, uint256 writePerByte, uint256 GPUTPerCycle, uint256 CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactor) CreateInvoice(opts *bind.TransactOpts, height_start *big.Int, height_end *big.Int, user common.Address, keepPerByte *big.Int, writePerByte *big.Int, GPUTPerCycle *big.Int, CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.contract.Transact(opts, "createInvoice", height_start, height_end, user, keepPerByte, writePerByte, GPUTPerCycle, CPUTtPerCycle)
 }
 
-// CreateInvoice is a paid mutator transaction binding the contract method 0x0e7ccabc.
+// CreateInvoice is a paid mutator transaction binding the contract method 0x96ea8630.
 //
-// Solidity: function createInvoice(bytes32 hash, uint256 sum, uint256 voices) returns()
-func (_Ethdepositcontract *EthdepositcontractSession) CreateInvoice(hash [32]byte, sum *big.Int, voices *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.CreateInvoice(&_Ethdepositcontract.TransactOpts, hash, sum, voices)
+// Solidity: function createInvoice(uint256 height_start, uint256 height_end, address user, uint256 keepPerByte, uint256 writePerByte, uint256 GPUTPerCycle, uint256 CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractSession) CreateInvoice(height_start *big.Int, height_end *big.Int, user common.Address, keepPerByte *big.Int, writePerByte *big.Int, GPUTPerCycle *big.Int, CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.CreateInvoice(&_Ethdepositcontract.TransactOpts, height_start, height_end, user, keepPerByte, writePerByte, GPUTPerCycle, CPUTtPerCycle)
 }
 
-// CreateInvoice is a paid mutator transaction binding the contract method 0x0e7ccabc.
+// CreateInvoice is a paid mutator transaction binding the contract method 0x96ea8630.
 //
-// Solidity: function createInvoice(bytes32 hash, uint256 sum, uint256 voices) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactorSession) CreateInvoice(hash [32]byte, sum *big.Int, voices *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.CreateInvoice(&_Ethdepositcontract.TransactOpts, hash, sum, voices)
+// Solidity: function createInvoice(uint256 height_start, uint256 height_end, address user, uint256 keepPerByte, uint256 writePerByte, uint256 GPUTPerCycle, uint256 CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactorSession) CreateInvoice(height_start *big.Int, height_end *big.Int, user common.Address, keepPerByte *big.Int, writePerByte *big.Int, GPUTPerCycle *big.Int, CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.CreateInvoice(&_Ethdepositcontract.TransactOpts, height_start, height_end, user, keepPerByte, writePerByte, GPUTPerCycle, CPUTtPerCycle)
 }
 
-// Deposit is a paid mutator transaction binding the contract method 0xb6b55f25.
+// DepositWithNodes is a paid mutator transaction binding the contract method 0x76c58850.
 //
-// Solidity: function deposit(uint256 resource) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactor) Deposit(opts *bind.TransactOpts, resource *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.contract.Transact(opts, "deposit", resource)
+// Solidity: function depositWithNodes(address[] _nodes) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactor) DepositWithNodes(opts *bind.TransactOpts, _nodes []common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.contract.Transact(opts, "depositWithNodes", _nodes)
 }
 
-// Deposit is a paid mutator transaction binding the contract method 0xb6b55f25.
+// DepositWithNodes is a paid mutator transaction binding the contract method 0x76c58850.
 //
-// Solidity: function deposit(uint256 resource) returns()
-func (_Ethdepositcontract *EthdepositcontractSession) Deposit(resource *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.Deposit(&_Ethdepositcontract.TransactOpts, resource)
+// Solidity: function depositWithNodes(address[] _nodes) returns()
+func (_Ethdepositcontract *EthdepositcontractSession) DepositWithNodes(_nodes []common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.DepositWithNodes(&_Ethdepositcontract.TransactOpts, _nodes)
 }
 
-// Deposit is a paid mutator transaction binding the contract method 0xb6b55f25.
+// DepositWithNodes is a paid mutator transaction binding the contract method 0x76c58850.
 //
-// Solidity: function deposit(uint256 resource) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactorSession) Deposit(resource *big.Int) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.Deposit(&_Ethdepositcontract.TransactOpts, resource)
+// Solidity: function depositWithNodes(address[] _nodes) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactorSession) DepositWithNodes(_nodes []common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.DepositWithNodes(&_Ethdepositcontract.TransactOpts, _nodes)
+}
+
+// ProposePricing is a paid mutator transaction binding the contract method 0x3296972f.
+//
+// Solidity: function proposePricing(uint256 _keepPerByte, uint256 _writePerByte, uint256 _GPUTPerCycle, uint256 _CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactor) ProposePricing(opts *bind.TransactOpts, _keepPerByte *big.Int, _writePerByte *big.Int, _GPUTPerCycle *big.Int, _CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.contract.Transact(opts, "proposePricing", _keepPerByte, _writePerByte, _GPUTPerCycle, _CPUTtPerCycle)
+}
+
+// ProposePricing is a paid mutator transaction binding the contract method 0x3296972f.
+//
+// Solidity: function proposePricing(uint256 _keepPerByte, uint256 _writePerByte, uint256 _GPUTPerCycle, uint256 _CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractSession) ProposePricing(_keepPerByte *big.Int, _writePerByte *big.Int, _GPUTPerCycle *big.Int, _CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.ProposePricing(&_Ethdepositcontract.TransactOpts, _keepPerByte, _writePerByte, _GPUTPerCycle, _CPUTtPerCycle)
+}
+
+// ProposePricing is a paid mutator transaction binding the contract method 0x3296972f.
+//
+// Solidity: function proposePricing(uint256 _keepPerByte, uint256 _writePerByte, uint256 _GPUTPerCycle, uint256 _CPUTtPerCycle) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactorSession) ProposePricing(_keepPerByte *big.Int, _writePerByte *big.Int, _GPUTPerCycle *big.Int, _CPUTtPerCycle *big.Int) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.ProposePricing(&_Ethdepositcontract.TransactOpts, _keepPerByte, _writePerByte, _GPUTPerCycle, _CPUTtPerCycle)
+}
+
+// RegisterNode is a paid mutator transaction binding the contract method 0x672d7a0d.
+//
+// Solidity: function registerNode(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactor) RegisterNode(opts *bind.TransactOpts, addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.contract.Transact(opts, "registerNode", addr)
+}
+
+// RegisterNode is a paid mutator transaction binding the contract method 0x672d7a0d.
+//
+// Solidity: function registerNode(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractSession) RegisterNode(addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.RegisterNode(&_Ethdepositcontract.TransactOpts, addr)
+}
+
+// RegisterNode is a paid mutator transaction binding the contract method 0x672d7a0d.
+//
+// Solidity: function registerNode(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactorSession) RegisterNode(addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.RegisterNode(&_Ethdepositcontract.TransactOpts, addr)
 }
 
 // Withdraw is a paid mutator transaction binding the contract method 0x51cff8d9.
 //
-// Solidity: function withdraw(address user) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactor) Withdraw(opts *bind.TransactOpts, user common.Address) (*types.Transaction, error) {
-	return _Ethdepositcontract.contract.Transact(opts, "withdraw", user)
+// Solidity: function withdraw(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactor) Withdraw(opts *bind.TransactOpts, addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.contract.Transact(opts, "withdraw", addr)
 }
 
 // Withdraw is a paid mutator transaction binding the contract method 0x51cff8d9.
 //
-// Solidity: function withdraw(address user) returns()
-func (_Ethdepositcontract *EthdepositcontractSession) Withdraw(user common.Address) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.Withdraw(&_Ethdepositcontract.TransactOpts, user)
+// Solidity: function withdraw(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractSession) Withdraw(addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.Withdraw(&_Ethdepositcontract.TransactOpts, addr)
 }
 
 // Withdraw is a paid mutator transaction binding the contract method 0x51cff8d9.
 //
-// Solidity: function withdraw(address user) returns()
-func (_Ethdepositcontract *EthdepositcontractTransactorSession) Withdraw(user common.Address) (*types.Transaction, error) {
-	return _Ethdepositcontract.Contract.Withdraw(&_Ethdepositcontract.TransactOpts, user)
+// Solidity: function withdraw(address addr) returns()
+func (_Ethdepositcontract *EthdepositcontractTransactorSession) Withdraw(addr common.Address) (*types.Transaction, error) {
+	return _Ethdepositcontract.Contract.Withdraw(&_Ethdepositcontract.TransactOpts, addr)
 }
--- contracts/VelasTest.sol
+++ contracts/VelasTest.sol
@@ -1,20 +1,89 @@
-pragma solidity ^0.5.16;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract VelasTest {
-    function() payable {
-
+    struct Pricing {
+        uint keepPerByte;
+        uint writePerByte;
+        uint GPUTPerCycle;
+        uint CPUTtPerCycle;
+        bool isChanged; //was it changed by the customer;
     }
 
-    function depositWithNodes(bytes32[] memory nodes) payable {
+    Pricing defaultPricing; //default market pricing
 
+    constructor() public {
+        defaultPricing.keepPerByte = 1;
+        defaultPricing.writePerByte = 1;
+        defaultPricing.GPUTPerCycle = 1;
+        defaultPricing.CPUTtPerCycle = 1;
+        defaultPricing.isChanged = false;
     }
 
-    function createInvoice(uint height_start, uint height_end, address user, uint spent) public  {
-
+    struct Node {
+        address addr;
+        uint balance;
+        bool active;
     }
 
-    function withdraw(address user) public {
+    mapping(address => Node) nodes;
 
+    struct Customer {
+        Pricing pricing;
+        mapping(address => Node) nodes;
+        uint balance;
     }
 
+    mapping(address => Customer) customers;
+
+    function() external payable {
+        Customer storage current = customers[msg.sender];
+        current.balance += msg.value;
+        current.pricing = defaultPricing;
+    }
+
+    //Customer may want to increase the price to be first in list
+    function proposePricing(uint _keepPerByte, uint _writePerByte, uint _GPUTPerCycle, uint _CPUTtPerCycle) public {
+        Customer storage current = customers[msg.sender];
+        current.pricing.keepPerByte = _keepPerByte;
+        current.pricing.writePerByte = _writePerByte;
+        current.pricing.GPUTPerCycle = _GPUTPerCycle;
+        current.pricing.CPUTtPerCycle = _CPUTtPerCycle;
+        defaultPricing.isChanged = true;
+
+        //TODO need to implement the sorting of customers to provide the priority list for resources;
+    }
+
+    function depositWithNodes(address[] memory _nodes) public payable {
+        Customer storage current = customers[msg.sender];
+        current.balance += msg.value;
+
+        if (defaultPricing.isChanged == false)
+            current.pricing = defaultPricing;
+
+        //current.nodes = nodes;
+    }
+
+    struct Invoice {
+        Pricing height_start;
+        Pricing height_end;
+        address user;
+        Pricing pricing;
+    }
+
+    //node sends the invoice to decrease the balance of the customer
+    function createInvoice(uint height_start, uint height_end, address user, uint keepPerByte, uint writePerByte, uint GPUTPerCycle, uint CPUTtPerCycle) public  {
+            //TODO here need to implement the logic of node voting and result verification by 2/3 of voices
+    }
+
+    function registerNode(address addr) public {
+        //TODO need to check if it exists
+        nodes[addr].active = true;
+    }
+
+    function withdraw(address addr) public {
+        Node storage node = nodes[addr];
+        require(node.balance > 0);
+        msg.sender.transfer(node.balance);
+        node.balance = 0;
+    }
 }
